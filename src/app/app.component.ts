127895b4812668a56eed7ea6c45438b79c50511f
dannyLiu <h152074716@gmail.com>
2022-09-01 15:50:01 +0800

    補充：簡化取得userGroup的方式、資料驗證的錯誤原因判斷

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,18 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   signupForm: FormGroup;
-
+  get userData(): FormGroup {
+    return this.signupForm.get('userData') as FormGroup;
+  }
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
         username: new FormControl(null, Validators.required),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(null, [
+          Validators.maxLength(5),
+          Validators.email,
+          Validators.required,
+        ]),
       }),
       gender: new FormControl('male'),
     });
